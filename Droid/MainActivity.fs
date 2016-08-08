8874f020317277d90449508f320736b6a7c85c75
Sam Williams <willsam100@gmail.com>
2016-08-09 06:56:46 +1200

    Initial commit

---
+++ Droid/MainActivity.fs
@@ -0,0 +1,22 @@
+﻿namespace ImageSingle.Droid
+open System;
+
+open Android.App;
+open Android.Content;
+open Android.Content.PM;
+open Android.Runtime;
+open Android.Views;
+open Android.Widget;
+open Android.OS;
+
+[<Activity (Label = "ImageSingle.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = (ConfigChanges.ScreenSize ||| ConfigChanges.Orientation))>]
+type MainActivity() =
+    inherit Xamarin.Forms.Platform.Android.FormsApplicationActivity()
+    override this.OnCreate (bundle: Bundle) =
+        base.OnCreate (bundle)
+
+        Console.WriteLine("activity is starting")
+
+        Xamarin.Forms.Forms.Init (this, bundle)
+        this.LoadApplication (new ImageSingle.App ())
+
---
+++ ImageSingle/ImageSingle.fs
@@ -0,0 +1,13 @@
+﻿namespace ImageSingle
+
+open Xamarin.Forms
+
+type App() = 
+    inherit Application()
+    let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
+    let image = Image(Aspect = Aspect.AspectFill)
+    do 
+        let imageSource = ImageSource.FromFile("icon.png")
+        image.Source <- imageSource
+        stack.Children.Add(image)
+        base.MainPage <- ContentPage(Content = stack)
